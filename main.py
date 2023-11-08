886d41b58e056109cd2b18511f307acba64094bc
Ethan Shi <ethanshi322@gmail.com>
2023-11-07 23:21:26 -0700

    Initial commit

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ def main():
     findContours()
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # ldjkld
     pass
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
