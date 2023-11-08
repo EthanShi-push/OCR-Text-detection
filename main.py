4ad348c48b2ed169734762a2b00a3d1b8e6b1161
Ethan Shi <ethanshi322@gmail.com>
2023-11-07 23:20:51 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+import numpy as np
+
+def findContours():
+    pass
+def main():
+
+    findContours()
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pass
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
