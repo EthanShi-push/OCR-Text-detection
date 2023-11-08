c881c80d4d6c0a8f0d5fa711b9af410400032dc4
Ethan Shi <ethanshi322@gmail.com>
2023-11-07 23:48:32 -0700

    Testing githu

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ def main():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     pass
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# just for testing
